f2e36703338c06b913afd0b966b091a8196bc5a7
santiago ocampo jimenez <santiagocampo163@gmail.com>
2021-10-04 21:54:15 -0500

    primer commit

---
+++ src/main/java/com/intermediary/IntermediaryApplication.java
@@ -0,0 +1,13 @@
+package com.intermediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntermediaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntermediaryApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/intermediary/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.intermediary;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IntermediaryApplication.class);
+	}
+
+}
