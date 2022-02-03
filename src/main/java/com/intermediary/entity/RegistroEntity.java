06183a7f3b425915b79b765da08cb596925b3551
santiagoocj <santiagocampo163@gmail.com>
2022-02-03 18:38:28 -0500

    feat: se crea relación entre registro y estado registro

--- src/main/java/com/intermediary/entity/RegistroEntity.java
+++ src/main/java/com/intermediary/entity/RegistroEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "registro")
+@Table(name = "registros")
 public class RegistroEntity implements Serializable{
 	/**
 	 * 
