2348876d8b08683243d8c61e5f58dbcfc8cd2b9c
santiagoocj <santiagocampo163@gmail.com>
2022-03-04 18:18:19 -0500

    refactor: se corrige error en el xml con plugin maven.org y se crea data de prueba en import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,10 @@ INSERT INTO representante_legal (id, tipo_documento, documento, nombre, apellido
 INSERT INTO representante_legal (id, tipo_documento, documento, nombre, apellidos, celular, email) VALUES (2, 'CC','15389649', 'yojan', 'ocampo jimenez', '3136199907', 'yojan@gmail.com');
 
 INSERT INTO categorias (id, tipo_categoria) VALUES (9,'muebles');
+
+/*MEMBRESIAS*/
+INSERT INTO membresias (id, suscripcion, vigencia) VALUES (1, 'Membresia express','15 dias');
+INSERT INTO membresias (id, suscripcion, vigencia) VALUES (2, 'Membresia premium','1 año');
+
+/*REGISTROS*/
+
